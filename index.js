a923029fedee799944a7a5c5e03b569f07b18c3d
Suman Sourabh <sumsourabh14@gmail.com>
2025-06-29 00:30:15 +0530

    added mongoose package

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3003;
+
+cors({ origin: process.env.FRONTEND_URI });
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`listening on port ${port}`);
 });
