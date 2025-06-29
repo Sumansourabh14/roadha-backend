d21cfd34c55e777430714610fbef0ecce207ec4c
Suman Sourabh <sumsourabh14@gmail.com>
2025-06-29 13:25:31 +0530

    added middlewares

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const connectDB = require("./db");
 const app = express();
 const port = process.env.PORT || 3003;
 
-app.use(cors({ origin: process.env.FRONTEND_URI }));
+app.use(cors({ origin: process.env.FRONTEND_URI, credentials: true }));
+
+app.use(express.json({ limit: "16kb" })); // handle JSON
+app.use(express.urlencoded({ extended: true, limit: "16kb" })); // handle url having '+' and '%20' terms
+app.use(express.static("public"));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
